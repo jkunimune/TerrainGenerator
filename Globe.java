d7aa9b3e8843b5a7fe3cb482e94517fa48e194e7
Justin Kunimune <jkunimune15@punahou.edu>
2015-02-02 11:53:04 -1000

    Initial Commit
    I made the bare bones of my to-be-terrain generator.  I might need to
    do a bit more research of graphics stuff, but right now, I really just
    want to test out my ideas for spherical stuff.

---
+++ Globe.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import java.awt.font.*;
+import java.awt.geom.*;
+import java.awt.image.BufferedImage;
+
+
+
+public class Globe {
+  private Tile[][] map;
+  private int radius;
+  
+  
+  
+  public Globe(int r) {
+  }
+  
+  
+  
+  public Color[][] mercator() {
+    return null;
+  }
+}
---
+++ Tile.java
@@ -0,0 +1,11 @@
+public class Tile {
+  public int altitude;
+  public int biome;
+  
+  
+  
+  public Tile() {
+    altitude = 0;
+    biome = 0;
+  }
+}
