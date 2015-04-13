a821a95d6387fe1d015fc070efe9956b44760733
Justin Kunimune <jkunimune15@punahou.edu>
2015-04-13 12:23:47 -1000

    Narrative and Terrain Generation
    I wrote my narrative (75 min) and spiffed up the original
    TerrainGenerator file a bit. (10 min)

--- TerrainGenerator.java
+++ TerrainGenerator.java
@@ -1,67 +1,34 @@
 public final class TerrainGenerator{ // a class to generate and display terrain onto a spherical surface
   static long startTime = 0; // the time it was when we last checked
+  static java.applet.AudioClip music; // the song that plays
   
   
   
   public static final void main(String args[]) {
+    startMusic();
+    
     while (true) {
       Planet earth = new Planet(100);
       Map theMap = new Hemispherical(earth, 800, 400);
       
       theMap.display(Map.altitude);
       
-      generate(earth, theMap);
+      earth.generate(theMap);
       
       System.out.println("end");
-      delay(20000);
+      delay(10000);
     }
   }
   
   
-  /* PRECONDITION: map's Globe is world */
-  public static final void generate(Planet world, Map map) { // randomly generates a map and simultaneously displays it
-    map.display(Map.altitude);
-    System.out.println("Generating landmasses...");
-      
-    world.spawnFirstContinent();
-    
-    int t = 0;
-    while (world.any(-257)) {
-      world.spawnContinents();
-      if (t%10 == 0)
-        map.display(Map.altitude);
-      t ++;
+  public static final void startMusic() {
+    try {
+      music = java.applet.Applet.newAudioClip(new java.net.URL("file:Sound/terraNova.wav"));
     }
-    if (t%10 != 1)
-      map.display(Map.altitude);
-    
-    System.out.println("Shifting continents...");
-    world.plateTechtonics();
-    map.display(Map.altitude);
-    
-    System.out.println("Roughing up and smoothing down terrain...");
-    for (int i = 64; i >= 1; i >>= 2) { // gradually randomizes and smooths out terrain
-      for (int j = 0; j < i; j ++)
-        world.smooth(.4);
-      map.display(Map.altitude);
-      world.rough(i*1.2);
-      map.display(Map.altitude);
+    catch (java.net.MalformedURLException error) {
+      System.out.println(error);
     }
-    
-    System.out.println("Generating climate...");
-    world.acclimate(.1);
-    world.climateEnhance();
-    map.display(Map.temperature);
-    map.display(Map.rainfall);
-    
-    System.out.println("Raining...");
-    world.rain();
-    world.runoff();
-    map.display(Map.water);
-    
-    System.out.println("Setting up biomes...");
-    world.biomeAssign();
-    map.display(Map.biome);
+    music.loop();
   }
   
   
