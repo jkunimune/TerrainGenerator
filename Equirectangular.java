9bba4228db1d333106e03d54127909623b6a56d2
Justin Kunimune <jkunimune15@punahou.edu>
2015-05-19 11:26:53 -1000

    Dotted Lines
    I fixed some bugs and tweaked some numbers.

--- Equirectangular.java
+++ Equirectangular.java
@@ -6,7 +6,7 @@ public class Equirectangular extends Map { // a simple globe projection that is 
   
   
   public final int getLat(int x, int y) {
-    if ((x>>1<<1 == width()/10>>1<<1 || x<<1>>1 == width()*9/10>>1<<1) && y/7%2 == 0) // draw dotted lines on sides
+    if ((x>>1<<1 == width()/10>>1<<1 || x>>1<<1 == width()*9/10>>1<<1) && y/7%2 == 0) // draw dotted lines on sides
       return -1;
     else
       return glb.latIndex(y*Math.PI/height());
@@ -14,7 +14,7 @@ public class Equirectangular extends Map { // a simple globe projection that is 
   
   
   public final int getLon(int x, int y) {
-    if ((x>>1<<1 == width()/10>>1<<1 || x<<1>>1 == width()*9/10>>1<<1) && y/7%2 == 0) // draw dotted lines on sides
+    if (lats[y][x] == -1) // draw dotted lines on sides
       return 0;
     else
       return glb.lonIndex(lats[y][x], (x*5/4)%width()*2*Math.PI/width());
--- Mercator.java
+++ Mercator.java
@@ -9,12 +9,12 @@ public class Mercator extends Map { // a sphere projection designed to preserve 
     if ((x>>1<<1 == width()/10>>1<<1 || x>>1<<1 == width()*9/10>>1<<1) && y/7%2 == 0) // draw dotted lines on sides
       return -1;
     else
-      return glb.latIndex(Math.PI/2 + Math.atan(((double)y-height()*5/8) / (width()/2/Math.PI)));
+      return glb.latIndex(Math.PI/2 + Math.atan(((double)y-height()/2.0) / (width()/(2*Math.PI))));
   }
   
   
   public final int getLon(int x, int y) {
-    if ((x>>1<<1 == width()/10>>1<<1 || x>>1<<1 == width()*9/10>>1<<1) && y/7%2 == 0) // draw dotted lines on sides
+    if (lats[y][x] == -1) // draw dotted lines on sides
       return 0;
     else
       return glb.lonIndex(lats[y][x], (x*5/4)%width() * 2*Math.PI / width());
