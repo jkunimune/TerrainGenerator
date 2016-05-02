b47d29fddc64a4a249fd4b1b9286112ae7e6e74b
Justin Kunimune <justinkunimune@gmail.com>
2016-05-02 01:05:23 -0400

    ?
    GitHub just decided not to commit this one change.

--- src/mechanics/IslandGeneration.java
+++ src/mechanics/IslandGeneration.java
@@ -19,7 +19,7 @@ public final class IslandGeneration {
 		while (true) {
 			Island atlantis = new Island(129);
 			
-			Map view = new Basic(atlantis.getSurface(), 500,500);
+			Map view = new Isometric(atlantis.getSurface(), 500,500);
 			
 			atlantis.generate(view);
 			
